b360e08ad8711dfe33804b12aaaf35a32236441d
cyanmallow <ggmmallow@gmail.com>
2024-04-15 23:50:41 +0700

    added nav bar

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+
+document.getElementById("open-sidebar").addEventListener("click", function(){
+    document.getElementById("sidebarId").style.display = "block";
+})
+document.getElementById("close-sidebar").addEventListener("click", function(){
+    document.getElementById("sidebarId").style.display = "none";
+})
